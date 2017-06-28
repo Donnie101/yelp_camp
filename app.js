ad9d45928a843d75018f7adb4000cf797b1391c0
L <L>
2017-06-28 11:27:27 +0000

    log this

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(methodOverride('_method'));
 app.use(flash());
 
 //seedDb();
-window.alert('YOU ARE A PUSSY');
+console.log(this);
 //PASSWORD CONFIGURATION
 app.use(require('express-session')({
   secret:'You nibbas think you are better than me.',
